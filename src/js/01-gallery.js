3258e1b4b487599838944b587110d1918dea5a05
Kushnierova <Katerynakushnierova@gmail.com>
2023-05-16 11:46:08 +0200

    11:46

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
 
-console.log(galleryItems);
+// Change code below this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import '../css/common.css';
+import '../css/01-gallery.css';
+
+const gallery = document.querySelector(".gallery");
+
+function renderList() {
+  const markup = galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}" />
+        </a>
+        </li>`
+    )
+    .join("");
+  gallery.innerHTML = markup;
+}
+renderList();
+
+new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
+
+  console.log(galleryItems);
